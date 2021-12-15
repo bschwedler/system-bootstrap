bab34c17d58f2afb95b07deaddf04574be7e7f12
Benjamin R. J. Schwedler <bschwedler@gmail.com>
2021-12-15 10:34:04 -0600

    Remove homebrew and other MacOS tasks

--- system-bootstrap.sh
+++ system-bootstrap.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-if [ "$(uname)" == "Darwin" ]; then
-  if [ -z "$(command -v brew)" ]; then
-    # Work around Objective-C fork() changes in 10.13
-    export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
-
-    echo "Install Homebrew & Ansible"
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    # py3 is now default
-    brew upgrade python || brew install python
-    brew unlink python && brew link python --overwrite
-  fi
-  /usr/local/bin/pip3 install --upgrade pip setuptools wheel
-  /usr/local/bin/pip3 install --upgrade --install-option="--prefix=/usr/local" ansible
-fi
-
 if [ -f "/etc/fedora-release" ] ; then
   sudo dnf install python3-devel python3-pip gcc libffi-devel openssl-devel
   sudo env "PATH=${PATH}" /usr/bin/pip3 install --upgrade pip setuptools wheel
